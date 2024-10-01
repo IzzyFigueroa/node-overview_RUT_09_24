f8c9ece2b7216392f47792df33f76ecffd4a90c0
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-10-01 12:48:45 -0400

    finished array methods

--- index.js
+++ index.js
@@ -1,35 +1,187 @@
-const mathTools = {
-    num1: 15,
-    num2: 10,
-    sum: function() { //shot hand would be to remove function and : example would be sym() {}
-         return this.num1 + this.num2;
-            
+const nums =[10, 3, 100, 70, 8, 55, 88]
+const users = [
+    {
+        name: 'bob',
+        age: 99
     },
-    difference: function() {
-        return this.num1 - this.num2;
+    {
+        name:'jim',
+        age: 25
+    },
+    {
+        name: 'sarah',
+        age: 40
+    }
+];
+
+
+const students = [
+    {
+      name: 'Roman',
+      average: 99
+    },
+    {
+      name: 'Matt',
+      average: 80
+    },
+    {
+      name: 'Izzy',
+      average: 95
+    },
+    {
+      name: 'Kandyce',
+      average: 90
+    }
+  ]
+
+  const sum = nums.reduce((output, num) => {
+    return output + num
+  }, 0)
+ 
+// this was around 12:10pm 10/1/24
+const overallAverage = students.reduce((output, studentObj, index, arr) => {
+    if (index === arr.length - 1)
+        return (output + studentObj.average) / arr.length;
+    // if we are on tha last student, retuern the output divided by the array length.
+
+    // otherwise return output + the studentObj average
+    return output + studentObj.average;
+}, 0)
+
+
+const studentInfo = students.reduce((output, studentObj, index, arr) => {
+    output.overallAverage += studentObj.average;
+
+    if (index === arr.length - 1) {
+        output.overallAverage = output.overallAverage / arr.length;
+
     }
 
-}; 
-const result = mathTools.sum()
-console.log(result)
+    output.studentCount = arr.length;
 
-const resultDif = mathTools.difference()
-console.log(resultDif)
+    return output;
+}, {
+    overallAverage: 0,
+    studentCount: 0
+
+});
+
+console.log(studentInfo)
+
+//reduce array (0 is intial output)
+// const sum = nums.reduce((output, num) => {
+//     return output + num 
+// }, 0)
+//  console.log(sum);
 
 
 
 
 
 
-function printName(userName) {
-    const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+
+
+
+
+
+
+
+//
+const evenArray = nums.map((num) => {
+    if ((num % 2) ===0 ) {
+        return `${num} is even`
+    } else {
+        return `${num} is odd`
+    }
+    return num % 2
+});
+
+console.log(evenArray)
+
+
+
+
+
+
+
+
+
+
+// for (const num of nums) { 
+
+//     console.log(num);
+// }
     
-    const capName = capitalize(userName);
-    console.log(capName)
 
-}
-// testOne();
+// // New loop (higher order function - a fucntion that takes in another function)
+// // this function goes through each item in your Array
+// nums.forEach((num, index) => {
+// console.log(index);
+// });
+// // one downside of this loop, i cant break it. It will go through each time and will not stop. if break is needed. use standard for loop
+// // and for of loop
 
 
 
-// printName('sarah');
+
+
+
+
+
+
+
+
+
+
+ // Filtering out values/items
+const filteredNums = nums.filter((num) => {
+    if (num > 10) {
+        return true;
+    }
+});
+
+const filteredUsers = users.filter((userObj) => {
+    if (userObj.age > 30) {
+        return true;
+    }
+        
+})
+
+console.log(filteredUsers);
+
+
+console.log(filteredNums);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Algorithm challenge - Write a function that orders the array numbers from lowests to highest
+//  const sorted = nums.sort((currentNum, nextNum) => {
+//     return nextNum - currentNum;
+//  });
+
+//  // Create a var that stores a sorted userArray. In your callback function, return thr different between the current objects age and the next object age.
+
+// const sortedUsers = users.sort((currentObj, nextObj) => {
+//     return currentObj.age - nextObj.age;
+// })
+
+
+// // Create a var that stores the sorted users array and sorts the okjects by name. hint(all letter characters have a number that you can pull wih JS)
+
+//  console.log(sorted)
+//  console.log(sortedUsers)
+
+
